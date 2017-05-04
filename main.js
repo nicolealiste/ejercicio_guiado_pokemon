c70813cefdecb6c032d96f5c4aec678dd4794197
Nicole Aliste <nicole.alistecastillo@gmail.com>
2017-05-04 09:45:59 -0300

    method

--- main.js
+++ main.js
@@ -2,6 +2,13 @@ function Pokemon(nombre,color){
 	this.nombre= nombre
 	this.color=color
 
-	
+	this.nivelDeAmistad=0
+
+	this.mostrarPokemon=function(){
+		return("Hola, soy: "+this.nombre+" y soy de color: "+this.color)
+	}
+	this.aumentarAmistad=function(valor){
+		this.nivelDeAmistad=this.nivelDeAmistad + valor
+	}
 }
 const Pikachu= new Pokemon ("Pikachu","Amarillo")
