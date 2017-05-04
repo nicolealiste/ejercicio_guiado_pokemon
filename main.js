cc4d41da75e4c58c1f7ae2aa262795b4097954e8
Nicole Aliste <nicole.alistecastillo@gmail.com>
2017-05-04 09:57:08 -0300

    final code

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
 function Pokemon(nombre,color){
-	this.nombre= nombre
-	this.color=color
+	this.nombre= nombre;
+	this.color=color;
 
-	this.nivelDeAmistad=0
+	this.nivelDeAmistad=0;
+
+	this.vida=100;
+	this.poderDeAtaque=poderDeAtaque;
 
 	this.mostrarPokemon=function(){
-		return("Hola, soy: "+this.nombre+" y soy de color: "+this.color)
-	}
+		return("Hola, soy: "+this.nombre+" y soy de color: "+this.color);
+	};
 	this.aumentarAmistad=function(valor){
-		this.nivelDeAmistad=this.nivelDeAmistad + valor
+		this.nivelDeAmistad=this.nivelDeAmistad + valor;
+	};
+	this.atacar=function(Pokemon){
+		Pokemon.vida=Pokemon.vida-this.poderDeAtaque;
 	}
 }
-const Pikachu= new Pokemon ("Pikachu","Amarillo")
+const Pikachu= new Pokemon ("Pikachu","Amarillo");
